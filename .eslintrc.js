732eb7acf0058524c04219624e272d034c1940a5
Ugo Romi <ugo.romi@gmail.com>
2021-02-23 20:34:00 +0100

    Created separate custom preset for react and node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: ["eslint:recommended", "airbnb", "plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {},
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: ["eslint:recommended", "airbnb", "plugin:prettier/recommended"],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	rules: {},
 };
--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: ["plugin:node/recommended", "./.eslintrc.js"],
-  rules: {},
-  engines: {
-    node: ">=14.15.5",
-  },
+	extends: ["plugin:node/recommended", "./.eslintrc.js"],
+	rules: {},
+	engines: {
+		node: ">=14.15.5",
+	},
 };
